75d36b199bd8a4debdf29b545bf0af527ff020bc
Summer Feiler <summerpfeiler@gmail.com>
2019-02-12 18:25:12 -0600

    CLI Assignment Feb 11 2019

---
+++ assignment-CLI.sh
@@ -0,0 +1,17 @@
+cd ~/Documents
+mkdir Projects
+cd Projects
+mkdir Assignment-CLI
+cd Assignment-CLI
+mkdir Version-1
+mkdir Version-2
+cd Version-1
+touch index.html
+touch contact-us.html
+touch services.html
+touch about-us.html
+touch index-2.html
+touch services-2.html
+cd ..
+mv Version-1/index-2.html Version-2
+mv Version-1/services-2.html Version-2
